8c43732ddb4c74ff8a2e28b950cc07caddd7f6c0
nambinh a <nambinh2016@gmail.com>
2020-11-22 15:41:39 +0700

    update

--- DiaryModel.java
+++ DiaryModel.java
@@ -1,29 +1,99 @@
 package diary;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 public class DiaryModel {
-	private String sContent;
-	private String sDate;
-
-	public DiaryModel(String content,String date) {
-		// TODO Auto-generated constructor stub
-		this.sContent=content;
-		this.sDate=date;
+	BufferedReader bufferedReader=null;
+	BufferedWriter bufferedWriter=null;
+	
+	public DiaryModel() {
+		
 	}
 
-	public String getsContent() {
-		return sContent;
+	//Doc file diary.txt
+	public String Docfile() throws IOException {
+		String content="";
+		try
+		{
+			Reader reader=new FileReader("./src/diary/diary.txt");
+			bufferedReader = new BufferedReader(reader);
+			int i;
+			while((i=bufferedReader.read())!=-1) {
+				content+=(char)i;
+			}
+		}finally {
+			if(bufferedReader!=null) {
+				bufferedReader.close();
+			}
+		}
+		
+		return content;
 	}
-
-	public void setsContent(String sContent) {
-		this.sContent = sContent;
+	
+	//Ghi tiep vao file cu
+	public void Ghifilecu(String content,String date) throws IOException {
+		try {
+			Writer writer=new FileWriter("./src/diary/diary.txt",true);
+			bufferedWriter = new BufferedWriter(writer);
+			
+			if(checkDate(date)) {
+				bufferedWriter.write(content);
+				bufferedWriter.newLine();
+			}else {
+				bufferedWriter.write("------------------"+date+"------------------\n");
+				bufferedWriter.write(content);
+				bufferedWriter.newLine();
+			}
+		}finally {
+			if(bufferedWriter!=null) {
+				bufferedWriter.close();
+			}
+		}
 	}
-
-	public String getsDate() {
-		return sDate;
+	
+	//Ghi de vao file cu
+	public void Themfilemoi(String content) throws IOException {
+		try {
+			Writer writer=new FileWriter("./src/diary/diary.txt");
+			bufferedWriter = new BufferedWriter(writer);
+			bufferedWriter.write(content);
+		}finally {
+			if(bufferedWriter!=null) {
+				bufferedWriter.close();
+			}
+		}
 	}
-
-	public void setsDate(String sDate) {
-		this.sDate = sDate;
+	
+	public Boolean checkDate(String date) throws IOException {
+		try
+		{
+			Reader reader=new FileReader("./src/diary/diary.txt");
+			bufferedReader = new BufferedReader(reader);
+			String i;
+			while((i=bufferedReader.readLine()) != null) {
+				if(i.equals("------------------"+date+"------------------")) {
+					return true;
+				}
+			}
+		}finally {
+			if(bufferedReader!=null) {
+				bufferedReader.close();
+			}
+		}
+		return false;
 	}
 	
 
--- MainFrame.java
+++ MainFrame.java
@@ -2,10 +2,14 @@ package diary;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Insets;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import javafx.scene.layout.Border;
+
 import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
 import javax.swing.JToggleButton;
@@ -14,9 +18,14 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ItemListener;
+import java.io.IOException;
 import java.util.Calendar;
 import java.awt.event.ItemEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -26,10 +35,13 @@ public class MainFrame extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtDay;
 	
+	//Lay ngay thang hien tai
 	private Calendar calendar=Calendar.getInstance();
 	private int dayNow=calendar.get(Calendar.DATE);
 	private int monthNow=calendar.get(Calendar.MONTH)+1;
 	private int yearNow=calendar.get(Calendar.YEAR);
+	
+	private DiaryModel diary = new DiaryModel();
 	
 
 	/**
@@ -64,7 +76,10 @@ public class MainFrame extends JFrame {
 		contentPane.add(scrollPane);
 		
 		JTextArea txtaDiary = new JTextArea();
+		txtaDiary.setLineWrap(true);
 		scrollPane.setViewportView(txtaDiary);
+		txtaDiary.setMargin(new Insets(5,5,0,0));
+		
 		
 		txtDay = new JTextField();
 		txtDay.setEditable(false);
@@ -72,39 +87,75 @@ public class MainFrame extends JFrame {
 		contentPane.add(txtDay);
 		txtDay.setColumns(10);
 		txtDay.setText(dayNow+"/"+monthNow+"/"+yearNow);
+		txtDay.setMargin(new Insets(0, 5, 0, 0));
 		
-		JCheckBox chckbxGhi = new JCheckBox("Ghi");
-		chckbxGhi.setSelected(true);
-		chckbxGhi.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent arg0) {
-				if(arg0.getStateChange()==2) {
-					txtaDiary.setEditable(false);
-				}else {
-					txtaDiary.setEditable(true);
-				}
-			}
-		});
-		chckbxGhi.setBounds(8, 73, 113, 25);
-		contentPane.add(chckbxGhi);
 		
 		JCheckBox chckbxDoc = new JCheckBox("Doc");
 		chckbxDoc.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
+				try {
+					if(arg0.getStateChange()==1) //Neu checkbox duoc check
+					{
+						String content=diary.Docfile();
+						txtaDiary.setText(content);
+					}
+					else {
+						txtaDiary.setText("");
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
 				
 			}
 		});
 		chckbxDoc.setBounds(8, 109, 113, 25);
 		contentPane.add(chckbxDoc);
 		
+		JCheckBox chckbxGhi = new JCheckBox("Ghi");
+		chckbxGhi.setSelected(true);
+		chckbxGhi.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				if(arg0.getStateChange()==1) {
+					txtaDiary.setEditable(true);
+				}else {
+					txtaDiary.setEditable(false);
+				}
+			}
+		});
+		chckbxGhi.setBounds(8, 73, 113, 25);
+		contentPane.add(chckbxGhi);
+
+		
 		JButton btnSave = new JButton("Luu");
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String str=txtaDiary.getText();
+				String content=txtaDiary.getText();
+				String date=txtDay.getText();
+				if(chckbxGhi.isSelected()) {
+					int res=JOptionPane.showConfirmDialog(rootPane, "Ban co muon luu nhat ky?","Luu nhat ky",JOptionPane.YES_NO_OPTION);
+					if(res==0) {
+						try {
+							if(chckbxDoc.isSelected()) 
+							{
+								diary.Themfilemoi(content);
+							}else {
+								diary.Ghifilecu(content,date);
+								txtaDiary.setText("");
+							}
+							JOptionPane.showMessageDialog(rootPane, "Luu thanh cong");
+						}catch (Exception e) {
+							// TODO: handle exception
+							e.printStackTrace();
+						}
+					}
+				}else {
+					JOptionPane.showMessageDialog(rootPane, "Ban chua chon che do luu");
+				}
 			}
 		});
 		btnSave.setBounds(8, 164, 97, 25);
 		contentPane.add(btnSave);
-		
 		
 	}
 }
